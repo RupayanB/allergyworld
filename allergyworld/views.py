728c0858025f2bfe86c611bf4d7efe05896ff494
Rupayan Basu <basu.rupayan@gmail.com>
2014-03-08 03:25:55 -0500

    static map accurately reflects address

--- allergyworld/views.py
+++ allergyworld/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from allergyworld.models import Restaurant
+import re
 
 def search_form(request):
     return render(request, 'allergyworld/search_form.html')
@@ -22,5 +23,9 @@ def search(request):
     	return render(request, 'allergyworld/search_form.html', {})
 
 def details(request, r_id):
-	r = get_object_or_404(Restaurant, pk=r_id)
-	return render(request, 'allergyworld/details.html', {'restaurant':r})
+    r = get_object_or_404(Restaurant, pk=r_id);
+    addr = re.sub(r',',' ',r.address)
+    addr = re.sub(r'  ',' ',addr)
+    addr = re.sub(r'\s','+',addr)
+    mapsrc = "http://maps.googleapis.com/maps/api/staticmap?center="+addr+"&zoom=15&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&sensor=false&key=AIzaSyCexiGJQdCfhi2gQHD5GCYozlbAXji9TlM"
+    return render(request, 'allergyworld/details.html', {'restaurant':r,'map_src':mapsrc})
--- setup.sh
+++ setup.sh
@@ -4,3 +4,6 @@
 sudo apt-get install -y git
 sudo apt-get install python-pip
 pip install Django==1.6.2
+pip install django-csvimport
+
+# Use manage.py csvimport --mappings='' --model='app_label.model_name' importfile.csv
