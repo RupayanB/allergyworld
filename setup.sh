99f303a5dd032e95eb9c451a2b3a9ae398eac3e1
Rupayan Basu <basu.rupayan@gmail.com>
2014-03-08 01:32:10 -0500

    corrected command for pip in setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 # Simple setup.sh for configuring Ubuntu 12.04 LTS EC2 instance
 
 sudo apt-get install -y git
-sudo apt-get install python-git
+sudo apt-get install python-pip
 pip install Django==1.6.2
