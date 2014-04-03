10510fcabc3b37c3045afcf1d06086c63e3638c4
Rupayan Basu <basu.rupayan@gmail.com>
2014-04-03 00:22:51 -0400

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,17 @@
 # Simple setup.sh for configuring Ubuntu 12.04 LTS EC2 instance
 
 sudo apt-get install -y git
+git config --global user.name 'RupayanB'
+git config --global user.email basu.rupayan@gmail.com
+git clone https://github.com/RupayanB/allergyworld.git
+
 sudo apt-get install python-pip
 pip install Django==1.6.2
 pip install django-csvimport
 
-# Use manage.py csvimport --mappings='' --model='app_label.model_name' importfile.csv
+
+# setup django
+# chmod 744 manage.py
+# ./manage.py syncdb
+# ./manage.py csvimport --mappings='' --model='allergyworld.Restaurant' restaurants_coord.csv
+# ./manage.py runserver inet_ip:port
